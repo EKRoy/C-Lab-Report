86e0678c4eaa50a8f2590f7a6b2a19c152e12378
Eshwor Kumar Roy <ekroy16@gmail.com>
2025-12-02 17:33:03 +0600

    Refactor electric bill calculation and output
    02

--- Problem_10.cpp
+++ Problem_10.cpp
@@ -1,43 +1,36 @@
-/*Program to take input of a user name and consumed current units and print 
-an electric bill which billing criteria is as follows 
-i) First 50 units has minimum charge 100tk 
-ii) Next 200 units cost 2.50 TK/unit 
-iii) Next 250 units cost 3.50 TK/unit 
-iv) Units above 500 are charged at a rate 5.00TK/unit 
+/*Program to take input of a user name and consumed current units and print
+an electric bill which billing criteria is as follows
+i) First 50 units has minimum charge 100tk
+ii) Next 200 units cost 2.50 TK/unit
+iii) Next 250 units cost 3.50 TK/unit
+iv) Units above 500 are charged at a rate 5.00TK/unit
 Make sure you implement the idea using user-defined function. */
-#include<iostream>
-#include<cmath>
-#include <iomanip>
-#include <limits>
+
+#include <iostream>
 using namespace std;
-double electricBill(double units){
-    double bill=0.0;
+void currentBill(double units){
+    double bill;
     if(units<=50){
         bill=100;
     }
-    else if(units>50 && units<=250){
+    else if(units<=250){
         bill=100+(units-50)*2.5;
     }
-    else if(units>250 && units<=500){
-        bill=100+(200*2.5)+(units-250)*3.5;
+    else if(units<=500){
+        bill = 100 + 200* 2.5+(units-250)*3.5;
     }
-    else bill=100+(200*2.5)+250*3.5+(units-500)*5;
-    return bill;
+    else bill=100+200*2.5+250*3.5+(units-500)*5;
+    cout<<"Current Bill:"<<bill<<endl;
 }
-int main(){
-    int choice=1;
-    while(choice==1){
-        string first,last;
-        cout<<"Enter your first name and last name: ";
-        cin>>first>>last;
-        double units;
-        cout<<"Enter your consumed units: ";
-        cin>>units;
-        cout<<"Mr/ms. "<<first<<" "<<last<<endl;
-        cout<<fixed<<setprecision(2);
-        cout<<"Your Electric Bill is:"<<electricBill(units)<<"Tk."<<endl;
-        cout << "Do you want to put new values? (1=yes, 0=no): ";
-       cin>>choice;
-    }
-   return 0;
+int main()
+{
+    string first, last;
+    cout << "Enter your first name and last name: "<<endl;
+    cin >> first >> last;
+    cout<<first<<" "<<last<<endl;
+    double units;
+    cout<<"Enter your consumed units:"<<endl;
+    cin>>units;
+    currentBill(units);
+    return 0;
 }
