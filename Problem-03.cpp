8cca687f3b22549f736842b5036237df1a603b5b
Eshwor Kumar Roy <ekroy16@gmail.com>
2025-12-02 10:45:03 +0600

    Refactor sin calculation function and main logic

--- Problem-03.cpp
+++ Problem-03.cpp
@@ -1,35 +1,29 @@
-/*Calculate the value of sinx  using its series for some 
+/*Calculate the value of sinx  using its series for some
    given 𝑥 using WHILE or DO WHILE condition. */
-#include<iostream>
-#define _USE_MATH_DEFINES
-#include<cmath>
-#include <iomanip>
+#include <iostream>
+#include <cmath>
 using namespace std;
-void coseFunction(double x,int n){
-    n--;
-    x=x*(M_PI/180);
-    double fact=1,sum=x;
+void sinFunction(double x,int n){
+    n=n-1;
+    x=x*(3.1416/180);
+    double sum=x;
+    double fact=1;
     int i=1;
     while(i<=n){
         fact=fact*(2*i+1)*(2*i);
-        sum+=pow(-1,i)*pow(x,2*i+1)/fact;
-        i++;
+        sum=sum+(pow(-1,i)*pow(x,2*i+1))/fact;
+        i=i+1;
     }
-    cout << fixed << setprecision(10);
-    cout<<"The value of sin("<<x<<") is:"<<sum<<endl;
+    cout<<"summation:"<<sum<<endl;
 }
-int main(){
+int main()
+{
     int n;
     double x;
-    int y=1;
-    while(y==1){
-        cout<<"Enter the number of term(n>0)"<<endl;
-        cin>>n;
-        cout<<"Enter the value of x in degree."<<endl;
-        cin>>x;
-        coseFunction(x,n);
-        cout << "Do you want to put new values? (1=yes, 0=no): ";
-        cin>>y;
-    }
-   return 0;
+    cout << "Enter the value of n:" << endl;
+    cin >> n;
+    cout << "Enter the value of x (in degree.):" << endl;
+    cin >> x;
+    sinFunction(x, n);
+    return 0;
 }
