72e37ba4b84e0938632113a4b3e32b27c4658f77
Eshwor Kumar Roy <ekroy16@gmail.com>
2025-12-02 23:50:07 +0600

    Update Problem-07_B.cpp
    01

--- Problem-07_B.cpp
+++ Problem-07_B.cpp
@@ -1,32 +1,22 @@
-/*Program to find the factorial of any given number.  Write codes for both 
-with and with user defined function. */
+/*Program to find the factorial of any given number.  Write codes for both
+with and without user defined function. */
 
-#include<iostream>
-#include<cmath>
-#include <iomanip>
+#include <iostream>
 using namespace std;
-void calculateFact(int n){
-    long long fact=1;
-    for(int i=1;i<=n;i++){
-        fact*=i;
+void factorial(int n)
+{
+    long long fact = 1;
+    for (int i = 1; i <= n; i++)
+    {
+        fact *= i;
     }
-    cout<<"Factorial of given number is: "<<fact<<endl;
+    cout << "The factorial of the given number is: " << fact << endl;
 }
-int main(){
-    int choice=1;
-    while(choice==1){
-        int n;
-        cout<<"Enter the number: ";
-        cin>>n;
-        if(n<0){
-            cout<<"Factorial does not exists"<<endl;
-            cout<<"Please enter the positon number"<<endl;
-        }
-        else{
-            calculateFact(n);
-        }
-        cout << "Do you want to put new values? (1=yes, 0=no): ";
-        cin>>choice;
-    }
-   return 0;
+int main()
+{
+    int n;
+    cout << "Enter the positive number: ";
+    cin >> n;
+    factorial(n);
+    return 0;
 }
