32ce11535eda22312c256f14d95a3174997d57d3
Eshwor Kumar Roy <ekroy16@gmail.com>
2025-12-03 11:26:33 +0600

    Update Problem-06_A.cpp
    cxc

--- Problem-06_A.cpp
+++ Problem-06_A.cpp
@@ -1,31 +1,26 @@
-/*06.Program to check whether any given number is prime or not.
- Write codes for both without user defined function*/
-
-#include<iostream>
-#include<cmath>
-#include <iomanip>
+/*Program to check whether any given number is prime or not.
+ Write codes for both with user defined function*/
+#include <iostream>
 using namespace std;
-int main(){
-    int choice=1;
-    while(choice==1){
-        int n;
-        cout<<"Enter the number you check: ";
-        cin>>n;
-        bool isPrime=true;
-        if(n==1) isPrime=false;
-        for(int i=2;i*i<=n;i++){
-            if(n%i==0){
-                isPrime=false;
-                break;
-            }
-        }
-        if(isPrime){
-            cout<<"The Number("<<n<<") is Prime"<<endl; 
-        }
-        else cout<<"The Number("<<n<<") is not Prime"<<endl;
+int main()
+{
+	int n;
+	cout << "Enter the value of n:" << endl;
+	cin >> n;
+	bool flag = true;
+	for (int i = 2; i < n; i = i + 1)
+	{
+		if (n % i == 0)
+		{
+			flag = false;
+		}
+	}
+	if (flag == true)
+	{
+		cout << "is prime number" << endl;
+	}
+	else
+		cout << "is not prime number" << endl;
 
-         cout << "Do you want to put new values? (1=yes, 0=no): ";
-        cin>>choice;
-    }
-   return 0;
+	return 0;
 }
