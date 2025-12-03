25c39b64f6424796bb202c82bae4fc2462a72684
Eshwor Kumar Roy <ekroy16@gmail.com>
2025-12-03 19:45:11 +0600

    Update Problem_09_B.cpp
    fdf

--- Problem_09_B.cpp
+++ Problem_09_B.cpp
@@ -1,62 +1,51 @@
-/*Program to find the largest and smallest number that can be formed using 
-digits of a given number. Write codes for with user 
+/*Program to find the largest and smallest number that can be formed using
+digits of a given number. Write codes for with user
 defined function. */
-#include<iostream>
-#include<cmath>
-#include <iomanip>
+#include <iostream>
 using namespace std;
-void largestNumber(string s,int n){
-     for(int i=0;i<n-1;i++){
-            for(int j=i+1;j<n;j++){
-                if(s[i]<s[j]){
-                    int temp=s[i];
-                    s[i]=s[j];
-                    s[j]=temp;
-                }
+void largestNumber(string s, int n)
+{
+    for (int i = 0; i < n - 1; i++)
+    {
+        for (int j = i + 1; j < n; j++)
+        {
+            if (s[i] < s[j])
+            {
+                char temp = s[i];
+                s[i] = s[j];
+                s[j] = temp;
             }
         }
-    cout<<"The largest number is: "<<endl;
-    for(int i=0;i<n;i++){
-       cout<<s[i];
-     }
-    cout<<endl;
-}
-void smallestNumber(string s,int n){
-    for(int i=0;i<n-1;i++){
-            for(int j=i+1;j<n;j++){
-                if(s[i]>s[j]){
-                    int temp=s[i];
-                    s[i]=s[j];
-                    s[j]=temp;
-                }
-            }
-        }
-    cout<<"The smallest number is: "<<endl;
-     for(int i=0;i<n;i++){
-         cout<<s[i];
-     }
-     cout<<endl;
-}
-int main(){
-    int choice=1;
-    while(choice==1){
-        int num;
-        cout<<"Enter the number: ";
-        cin>>num;
-        string s=to_string(num);
-        int n=0;
-        while(num){
-            int x=num%10;
-            n++;
-            num/=10;
-        }
-        //largest number:we need descending order of the digits
-        largestNumber(s,n);
-       // smallest number:we need ascending order of the digits
-       smallestNumber(s,n);
-        cout << "Do you want to put new values? (1=yes, 0=no): ";
-        cin>>choice;
     }
-   return 0;
+    cout << "The largest number is: " << s << endl;
+}
 
+void smallestNumber(string s, int n)
+{
+    for (int i = 0; i < n - 1; i++)
+    {
+        for (int j = i + 1; j < n; j++)
+        {
+            if (s[i] > s[j])
+            {
+                char temp = s[i];
+                s[i] = s[j];
+                s[j] = temp;
+            }
+        }
+    }
+    cout << "The smallest number is: " << s << endl;
+}
+
+int main()
+{
+    string s;
+    cout << "Enter the number: ";
+    cin >> s;
+
+    int n = s.length();
+    largestNumber(s, n);
+    smallestNumber(s, n);
+
+    return 0;
 }
