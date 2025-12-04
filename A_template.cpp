188742ba77e5206abebe59289730948b9e9da276
Eshwor Kumar Roy <ekroy16@gmail.com>
2025-12-04 20:05:44 +0600

    Add files via upload
    fgf

---
+++ A_template.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+start:
+
+    int si;
+    cout << "Press 1 for Yes Or 0 for NO" << endl;
+    cin >> si;
+    if (si == 1)
+        goto start;
+    return 0;
+}
