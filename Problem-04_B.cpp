8167d16d557e5c2b1acc8bfed9f603639e388f69
Eshwor Kumar Roy <ekroy16@gmail.com>
2025-12-05 20:02:37 +0600

    do while loop(Not mandotory)

---
+++ Problem-04_B.cpp
@@ -0,0 +1,39 @@
+/*Calculate the value of cosx using its series for some
+  given 𝑥 using DO WHILE condition. */
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+void cosFunction(double x, int n)
+{
+    n = n - 1;
+    x = x * (3.1416 / 180);
+    double sum = 1;
+    double fact = 1;
+    int i = 1;
+
+    if (n > 0)
+    {
+        do
+        {
+            fact = fact * (2 * i - 1) * (2 * i);
+            sum = sum + (pow(-1, i) * pow(x, 2 * i)) / fact;
+            i = i + 1;
+        } while (i <= n);
+    }
+
+    cout << "summation: " << sum << endl;
+}
+
+int main()
+{
+    int n;
+    double x;
+    cout << "Enter the value of n:" << endl;
+    cin >> n;
+    cout << "Enter the value of x (in degree.):" << endl;
+    cin >> x;
+
+    cosFunction(x, n);
+    return 0;
+}
