0482fcd6b3b5b9e06a47e9cee15352c784617a6a
Eshwor Kumar Roy <ekroy16@gmail.com>
2025-12-05 19:58:38 +0600

    Do while Loop(Not mandatory)

---
+++ Problem-03_B.cpp
@@ -0,0 +1,42 @@
+/*Calculate the value of sinx  using its series for some
+   given 𝑥 using DO WHILE condition. */
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+void sinFunction(double x, int n)
+{
+    n = n - 1;
+    x = x * (3.1416 / 180);
+    double sum = x;
+    double fact = 1;
+    int i = 1;
+
+    if (n > 0)
+    {
+        do
+        {
+            fact = fact * (2 * i + 1) * (2 * i);
+            sum = sum + (pow(-1, i) * pow(x, 2 * i + 1)) / fact;
+            i = i + 1;
+        } while (i <= n);
+    }
+
+    cout << "summation: " << sum << endl;
+}
+
+int main()
+{
+    int n;
+    double x;
+
+    cout << "Enter the value of n:" << endl;
+    cin >> n;
+
+    cout << "Enter the value of x (in degree.):" << endl;
+    cin >> x;
+
+    sinFunction(x, n);
+
+    return 0;
+}
