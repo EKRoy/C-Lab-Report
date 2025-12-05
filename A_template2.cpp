1bccba2d4333e23fbe697d62661c6f275e9e1a2d
Eshwor Kumar Roy <ekroy16@gmail.com>
2025-12-05 19:37:18 +0600

    this template use copy to paste all code

---
+++ A_template2.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int si = 1;
+    while (si == 1)
+    {
+        // body
+
+        cout << "Press 1 for Yes Or 0 for NO" << endl;
+        cin >> si;
+    }
+    return 0;
+}
