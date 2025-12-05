b58cfa7db53d040f5dc8b556140435bde0bac717
Eshwor Kumar Roy <ekroy16@gmail.com>
2025-12-05 19:34:39 +0600

    Update A_template.cpp
    fd

--- A_template.cpp
+++ A_template.cpp
@@ -3,11 +3,12 @@ using namespace std;
 int main()
 {
 start:
-
+    //start main body
     int si;
     cout << "Press 1 for Yes Or 0 for NO" << endl;
     cin >> si;
     if (si == 1)
         goto start;
     return 0;
+
 }
