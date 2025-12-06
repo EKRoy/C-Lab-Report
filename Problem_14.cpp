151179e15e1117c21fc6baed72273c9a6c782311
Eshwor Kumar Roy <ekroy16@gmail.com>
2025-12-06 11:24:54 +0600

    Update Problem_14.cpp
    dfdf

--- Problem_14.cpp
+++ Problem_14.cpp
@@ -1,58 +1,64 @@
-/*Program to find the dominant eigenvalue and corresponding eigenvector of 
+/*Program to find the dominant eigenvalue and corresponding eigenvector of
 a given non-diagonal square matrix. */
 
-#include <iostream>
-#include <iomanip>
-#include <cmath>
-#include <vector>
+#include <bits/stdc++.h>
+#define t 0.00001
 using namespace std;
-int main() {
+int main()
+{
     cout << fixed << setprecision(4);
     int n;
     cout << "Enter the order of the matrix: ";
     cin >> n;
-    vector<vector<double>> A(n, vector<double>(n));
-    vector<double> b(n), c(n);
+    double A[n][n];
+    double v[n], c[n];
     cout << "Enter the elements of the matrix row-wise:\n";
     for (int i = 0; i < n; i++)
+    {
         for (int j = 0; j < n; j++)
+        {
             cin >> A[i][j];
-
+        }
+    }
     cout << "Enter the initial guess for the eigenvector:\n";
     for (int i = 0; i < n; i++)
-        cin >> b[i];
-
-    double eps;
-    cout << "Enter the desired accuracy: ";
-    cin >> eps;
-
-    double k = b[0]; // Initial guess for eigenvalue
+    {
+        cin >> v[i];
+    }
+    double k = v[0]; // Initial guess for eigenvalue
     double y;
-    do {
+    do
+    {
         y = k;
-        // Compute c = A * b
-        for (int i = 0; i < n; i++) {
+        // Compute c = A * v
+        for (int i = 0; i < n; i++)
+        {
             c[i] = 0;
             for (int j = 0; j < n; j++)
-                c[i] += A[i][j] * b[j];
+            {
+                c[i] = c[i] + A[i][j] * v[j];
+            }
         }
 
         // Estimate the eigenvalue as the max absolute value of c
         k = abs(c[0]);
         for (int i = 1; i < n; i++)
+        {
             if (abs(c[i]) > k)
+            {
                 k = abs(c[i]);
-
+            }
+        }
         // Normalize to get the next eigenvector
         for (int i = 0; i < n; i++)
-            b[i] = c[i] / k;
+            v[i] = c[i] / k;
 
-    } while (abs(k - y) >= eps);
+    } while (abs(k - y) >= t);
 
     cout << "\nThe largest Eigenvalue is: " << k << "\n";
     cout << "The corresponding Eigenvector is:\n";
     for (int i = 0; i < n; i++)
-        cout << b[i] << "\n";
+        cout << v[i] << "\n";
 
     return 0;
 }
