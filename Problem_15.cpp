e878274f20476286af1b55ff6068cf374b3eecb2
Eshwor Kumar Roy <ekroy16@gmail.com>
2025-12-06 18:17:07 +0600

    Update Problem_15.cpp

--- Problem_15.cpp
+++ Problem_15.cpp
@@ -1,15 +1,12 @@
-// Determinant using Row-Elementary Operations with vector
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-
 int main()
 {
-    cout << fixed << setprecision(2);
     int n;
-    cout << "\nEnter the dimension of the matrix: ";
+    cout << "\nEnter the order of matrix: ";
     cin >> n;
-    double a[100][100];
-    cout << "\nEnter the elements of the matrix row-wise:\n";
+    double a[100][101];
+    cout << "\nEnter the elements:\n";
     for (int i = 0; i < n; i++)
     {
         for (int j = 0; j < n; j++)
@@ -17,62 +14,69 @@ int main()
             cin >> a[i][j];
         }
     }
-    double determinant = 1;
 
-    // Pivotisation: make leading element the largest by absolute value
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = n; j < 2 * n; j++)
+        {
+            if (j == i + n)
+            {
+                a[i][j] = 1;
+            }
+            else
+                a[i][j] = 0;
+        }
+    }
+    // pivot
     for (int i = 0; i < n; i++)
     {
         for (int k = i + 1; k < n; k++)
         {
             if (abs(a[i][i]) < abs(a[k][i]))
             {
-                for (int j = 0; j < n; j++)
+                for (int j = 0; j < 2 * n; j++)
                 {
                     swap(a[i][j], a[k][j]);
-                    determinant = determinant * -1;
                 }
             }
         }
     }
 
-    cout << "\nThe matrix after Pivotisation is:\n";
     for (int i = 0; i < n; i++)
     {
-        for (int j = 0; j < n; j++)
+        for (int k = 0; k < n; k++)
+        {
+            if (k != i)
+            {
+                double factor = a[k][i] / a[i][i];
+                for (int j = 0; j < 2 * n; j++)
+                {
+                    a[k][j] = a[k][j] - factor * a[i][j];
+                }
+            }
+        }
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        double temp = a[i][i];
+        if (temp < 1e-9)
+        {
+            cout << "Does not exists" << endl;
+            return 0;
+        }
+        for (int j = 0; j < 2 * n; j++)
+        {
+            a[i][j] =a[i][j]/ temp;
+        }
+    }
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = n; j < 2 * n; j++)
         {
             cout << a[i][j] << " ";
         }
-        cout << "\n";
+        cout << endl;
     }
-
-    // Gaussian elimination to upper triangular matrix
-    for (int i = 0; i < n - 1; i++)
-    {
-        if (fabs(a[i][i]) < 1e-9)
-        {
-            cout << "Zero pivot encountered! Determinant is 0.\n";
-            return 0;
-        }
-        for (int k = i + 1; k < n; k++)
-        {
-            double factor = a[k][i] / a[i][i];
-            for (int j = 0; j < n; j++)
-                a[k][j] -= factor * a[i][j];
-        }
-    }
-
-    cout << "\nThe matrix after Gaussian Elimination is:\n";
-    for (int i = 0; i < n; i++)
-    {
-        for (int j = 0; j < n; j++)
-            cout << setw(10) << a[i][j];
-        cout << "\n";
-    }
-
-    // Calculate determinant from diagonal
-    for (int i = 0; i < n; i++)
-        determinant *= a[i][i];
-
-    cout << "\nThe required determinant is: " << determinant << "\n";
     return 0;
 }
