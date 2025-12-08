027319b7b86b828fb506ea6a25a3e770fdd212f0
Eshwor Kumar Roy <ekroy16@gmail.com>
2025-12-08 13:20:12 +0600

    Update Problem_20.cpp

--- Problem_20.cpp
+++ Problem_20.cpp
@@ -1,57 +1,51 @@
 /*write an OOP to find the roots of a general quadratic equation. */
-#include<iostream>
-#include<cmath>
-#include <iomanip>
+#include <bits/stdc++.h>
 using namespace std;
-class QuadraticEquation{
-    private:
-        double a,b,c;
-    //constructor
-    public:
-        QuadraticEquation(double coeffA, double coeffB, double coeffC) {
-        a = coeffA;
-        b = coeffB;
-        c = coeffC;
+class Quadratic
+{
+public:
+    double a, b, c;
+    void input()
+    {
+        cout << "Enter the coefficient of a,b,c ";
+        cin >> a >> b >> c;
     }
-    void findRoots(){
-        if(a==0){
-            cout<<"This is not a Quadratic equation(a==0)"<<endl;
+    void calculate()
+    {
+        if (a == 0)
+        {
+            cout << "This is not a Quadratic equation(a==0)" << endl;
             return;
         }
-        double dist=b*b-4*a*c;
-        if(dist>0){
-            double root1=(-b+sqrt(dist))/(2*a);
-            double root2=(-b-sqrt(dist))/(2*a);
-            cout<<"Roots are real and different."<<endl;
-            cout<<"Root1="<<root1<<endl;
-            cout<<"Root2="<<root2<<endl;
+        double dist = b * b - 4 * a * c;
+        if (dist > 0)
+        {
+            double root1 = (-b + sqrt(dist)) / (2 * a);
+            double root2 = (-b - sqrt(dist)) / (2 * a);
+            cout << "Roots are real and different." << endl;
+            cout << "Root1=" << root1 << endl;
+            cout << "Root2=" << root2 << endl;
         }
-        else if(dist==0){
-            double root=-b/(2*a);
-            cout<<"Roots are real and same."<<endl;
-             cout<<"Root1=Root2 = "<<root<<endl;
+        else if (dist == 0)
+        {
+            double root = -b / (2 * a);
+            cout << "Roots are real and same." << endl;
+            cout << "Root1=Root2 = " << root << endl;
         }
-        else{
-            double realPart=-b/(2*a);
-            double img=sqrt(-dist)/(2*a);
-            cout<<"Roots are complex and different."<<endl;
-            cout<<"Root1= "<<realPart<<"+ "<<img<<"i"<<endl;
-            cout<<"Root1= "<<realPart<<"- "<<img<<"i"<<endl;
-
+        else
+        {
+            double realPart = -b / (2 * a);
+            double img = sqrt(-dist) / (2 * a);
+            cout << "Roots are complex and different." << endl;
+            cout << "Root1= " << realPart << "+ " << img << "i" << endl;
+            cout << "Root2= " << realPart << "- " << img << "i" << endl;
         }
     }
 };
-int main(){
-    cout<<fixed<<setprecision(2);
-    int choice=1;
-    while(choice==1){
-        double a,b,c;
-        cout<<"Enter the coefficients of a,b,c: ";
-        cin>>a>>b>>c;
-        QuadraticEquation Q(a,b,c);
-        Q.findRoots();
-        cout << "Do you want to calculate another quadratic equation? (1=yes, 0=no): ";
-        cin>>choice;
-    }
-   return 0;
+int main()
+{
+    Quadratic Q;
+    Q.input();
+    Q.calculate();
+    return 0;
 }
